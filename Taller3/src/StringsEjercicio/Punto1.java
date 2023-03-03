184b13c944595e419fccb16f3ce6a0e7cf681dd3
vSurriola <urriola2611@gmail.com>
2023-03-03 13:27:41 -0500

    hola

---
+++ Taller3/src/StringsEjercicio/Punto1.java
@@ -0,0 +1,17 @@
+package StringsEjercicio;
+
+public class Punto1 {
+	public static void main(String[] args) {
+		
+		String myString = ("abc");
+		String myString1 = new String ("abc");
+		String myString2 = ("abc");
+		
+		System.out.println("El resultado de la primera cadena es:"+myString);
+		System.out.println("El resultado de la segunda cadena es:"+myString1);
+		System.out.println("El resultado de la tercer cadena es:"+myString2);
+		
+		
+	}
+
+}
---
+++ Taller3/src/StringsEjercicio/Punto3.java
@@ -0,0 +1,17 @@
+package StringsEjercicio;
+import java.util.Scanner;
+public class Punto3 {
+	
+	
+	public static void main(String[] args){
+	
+		String nombre = "Miguel ";
+		String apellido = "Urriola Arriaga y ";
+		 String edad = new String  ("tienes 18");
+	
+		
+		System.out.println("Tu  nombre es:   "	+nombre + apellido + edad);
+	
+	}//end class StringPractice
+
+}
